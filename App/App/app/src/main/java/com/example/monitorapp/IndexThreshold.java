35aaa62276be13431f175ca0262ab3b34a527b96
sisters789 <nguyengiabao442@gmail.com>
2021-11-15 22:42:07 +0700

    Add files via upload
    GB, frame2

---
+++ App/App/app/src/main/java/com/example/monitorapp/IndexThreshold.java
@@ -0,0 +1,54 @@
+package com.example.monitorapp;
+
+public class IndexThreshold {
+    private float value;
+    private boolean isGreater;
+    private boolean isUse;
+
+    public float getValue() {
+        return value;
+    }
+
+    public IndexThreshold() {
+        this.value = 0;
+        this.isGreater = false;
+        this.isUse = false;
+    }
+
+    public IndexThreshold(float value, boolean isGreater, boolean isUse) {
+        this.value = value;
+        this.isGreater = isGreater;
+        this.isUse = isUse;
+    }
+
+    @Override
+    public String toString() {
+        String res="";
+        if (isGreater)
+            res+=">=";
+        else
+            res+="<";
+        res+=value;
+        return res;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public boolean isGreater() {
+        return isGreater;
+    }
+
+    public void setGreater(boolean greater) {
+        isGreater = greater;
+    }
+
+    public boolean isUse() {
+        return isUse;
+    }
+
+    public void setUse(boolean use) {
+        isUse = use;
+    }
+}
